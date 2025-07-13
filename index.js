f19f6572ccdb42b78ce0a77148ed484d0ecd74e3
Chandan B <116443036+Chandanb2003@users.noreply.github.com>
2025-07-13 10:56:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-  res.send('👋 Hello from Chandan\'s Docker app!Justice For Sowjanya
-Justice for Dharmasthala Victims Public Appeal for Fair Investigation.');
+  res.send(`👋 Hello from Chandan's Docker app!
+Justice For Sowjanya
+Justice for Dharmasthala Victims
+Public Appeal for Fair Investigation.`);
 });
 
 app.listen(PORT, () => {
